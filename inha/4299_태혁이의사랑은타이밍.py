f2ef5bb7458fd270c4be4d9d7d7b2cb826c6296b
jih <jih_319@naver.com>
2021-09-08 08:51:48 +0900

    first commit

--- inha/4299_태혁이의사랑은타이밍.py
+++ inha/4299_태혁이의사랑은타이밍.py
@@ -2,12 +2,9 @@ T = int(input())
 
 for tc in range(1, T+1):
     D, H, M = map(int, input().split())
-    if D < 11:
-        result = -1
-    elif D == 11 and H < 11:
-        result = -1
-    elif D == 11 and H == 11 and M < 11:
-        result = -1
+    result = (D - 11) * 1440 + (H - 11) * 60 + (M - 11)
+    if result < 0:
+        timing = -1
     else:
-        result = (D-11)*1440 + (H-11)*60 + (M-11)
-    print('#{} {}'.format(tc, result))
+        timing = result
+    print('#{} {}'.format(tc, timing))
