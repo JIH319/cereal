dfe26d368d62b4e282919b8872b16227d6bbe55e
이수진 <whdlsj98@naver.com>
2021-09-17 19:26:29 +0900

    txt...

--- sujin/problem02/1874_baek_스택 수열.py
+++ sujin/problem02/1874_baek_스택 수열.py
@@ -1,3 +1,31 @@
+
+
+'''
+import sys
+N = int(sys.stdin.readline())
+data = ''
+for i in range(N):
+    data += sys.stdin.readline().rstrip()
+
+stack = ''
+result = ''
+dataidx = 0
+for j in range(1,N+1):
+    stack += str(j)
+    result += '+'
+    # 일단 넣고, 해당 숫자 (i)가 나올 순서면 pop 
+    while stack and stack[-1] == data[dataidx]:
+         stack = stack[:-1]
+         result += '-'
+         dataidx += 1
+
+if dataidx == N and not stack:
+    print('\n'.join(result))
+else:
+    print('NO')
+
+
+'''
 import sys
 N = int(sys.stdin.readline())
 data = ''
@@ -16,8 +44,13 @@ for j in range(1,N+1):
          result += '-'
          dataidx += 1
 
-if dataidx == N:
-    for k in range(2*N):
-        print(result[k])
+if dataidx == N and not stack:
+    print('\n'.join(result))
 else:
     print('NO')
+
+
+
+
+# for k in range(2*N):
+    #     print(result[k])
